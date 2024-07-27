bbb483c68b4c815b8de06589777a5e37794a78bb
Jacob Plotz <jacobplotz@knights.ucf.edu>
2024-07-27 15:27:12 -0400

    1 koan left (extra credit: play greed)

--- koans/dice-project.lisp
+++ koans/dice-project.lisp
@@ -18,16 +18,18 @@
 
 (defclass dice-set ()
   ;; Fill in the blank with a proper slot definition.
-  (____))
+  ((dice-values :initarg :dice-values :initform '() :accessor dice-values)))
 
 ;;; This method might be unnecessary, depending on how you define the slots of
 ;;; DICE-SET.
 
-(defmethod dice-values ((object dice-set))
-  ____)
+;;;(defmethod dice-values ((object dice-set))
+;;;  ____)
 
 (defmethod roll (count (object dice-set))
-  ____)
+  (check-type count (integer 1))
+  (setf (dice-values object)
+	(loop repeat count collect (1+ (random 6)))))
 
 (define-test make-dice-set
   (let ((dice (make-instance 'dice-set)))
